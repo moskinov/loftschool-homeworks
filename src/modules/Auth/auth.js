273e5cd9c70d79fe67b7bbfbeb5fcaa4adbaf9cf
moskinov <moskinov@proitr.ru>
2019-02-01 17:16:59 +0300

    homework-nasa-rover-viewer

--- src/modules/Auth/auth.js
+++ src/modules/Auth/auth.js
@@ -1,1 +1,21 @@
 // Реализуйте редьюсер
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import { addKey } from './actions';
+
+const apiKey = handleActions(
+	{
+		[addKey]: (_state, action) => action.payload
+	},
+	null
+);
+
+export default combineReducers({
+	apiKey
+});
+
+export const getIsAuthorized = (state) =>
+
+		state.auth.apiKey !== null ? true :
+		false;
+export const getApiKey = (state) => state.auth.apiKey;
--- src/modules/RoverPhotos/RoverPhotos.js
+++ src/modules/RoverPhotos/RoverPhotos.js
@@ -1,2 +1,56 @@
 // Реализуйте редьюсер
 // Файл с тестами RoverPhotos.test.js поможет вам в этом
+import { handleActions } from 'redux-actions';
+import { combineReducers } from 'redux';
+import {
+  fetchPhotosRequest,
+  fetchPhotosSuccess,
+  fetchPhotosFailure,
+  changeSol
+} from './actions';
+
+export const roversNames = ['curiosity', 'opportunity', 'spirit'];
+
+const photos = combineReducers(
+    roversNames.reduce((item, type) => {
+        item[type] = handleActions({
+            [fetchPhotosRequest]: (_state, action) => {
+            const { name, sol } = action.payload;
+            return name === type ? { ..._state, [sol]: { photos: null, isLoaded: false } } : _state;
+            },
+            [fetchPhotosSuccess]: (_state, action) => {
+            const { name, sol, photos } = action.payload;
+            return name === type ? { ..._state, [sol]: { photos, isLoaded: true } } : _state;
+            },
+            [fetchPhotosFailure]: (_state, action) => {
+            const { name, sol, error } = action.payload;
+            return name === type ? { ..._state, [sol]: { error, isLoading: false } } : _state;
+            }
+        },
+        {}
+    );
+    return item;
+  },
+  {})
+);
+
+const sol = handleActions(
+  { [changeSol]: (_state, action) => ({ ..._state, current: action.payload }) },
+  { current: 1, min: 1, max: 100 }
+);
+
+export default combineReducers({
+  photos,
+  sol
+});
+
+export const getSol = state => state.roverPhotos.sol;
+export const getRoversPhotos = state => state.roverPhotos.photos;
+export const getSavedPhotos = (state, name, id) => {
+  const { roverPhotos: { photos } } = state;
+  if (!photos[name][id]) {
+    return null;
+  }
+
+  return photos[name][id].photos;
+};
